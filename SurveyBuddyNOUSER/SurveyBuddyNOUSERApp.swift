b1bf472f7b9e9e76d42f9ceab50c5275c41713cc
Norbert <nor.szaw@gmail.com>
2023-12-11 14:52:06 +0100

    Initial Commit

---
+++ SurveyBuddyNOUSER/SurveyBuddyNOUSERApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyBuddyNOUSERApp.swift
+//  SurveyBuddyNOUSER
+//
+//  Created by Norbert Szawloga on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SurveyBuddyNOUSERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
